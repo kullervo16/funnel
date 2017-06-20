a655daaed2646ff6127d709ed02947c7dbc0a99a
Jef Verelst <kullervo16@gmail.com>
2017-06-20 20:37:22 +0200

    Placement of the entries... works up to 3... needs generalisation

--- src/funnel.js
+++ src/funnel.js
@@ -36,6 +36,35 @@ function createFunnel(divId, width, height, funnelData) {
         return headerBuffer + (height / 2);
     }
     
+    function addText(x, y,size, txt) {
+        svgContainer.append("text").attr("x", x)
+                                    .attr("y", y)
+                                    .text(txt)
+                                    .attr("font-family", "sans-serif")
+                                    .attr("font-size", size+"px")
+                                    .attr("fill", "black");
+    }
+    
+    /**
+     * This function will put an entry in a phase. It positions the first, then recurses.
+     * @param {type} entries the entries still to be treated
+     * @param {type} sequence the sequence in the phase (needed to calculate where to draw it)
+     * @returns {undefined}
+     */
+    function addEntriesToPhase(entries, phaseHeight,xBase, sequence) {
+        if(entries === undefined || entries.length === 0) {
+            return; // I've done my job... stop the recursion
+        }
+        var currentEntry = head(entries);
+        
+        var entryX = xBase + randomText(getPhaseWidth(), 20, currentEntry.name.length);
+        var entryY = getCenter() + getYPositionForSequence(sequence, phaseHeight);
+        
+        addText(entryX, entryY, 20, currentEntry.name);
+        
+        addEntriesToPhase(tail(entries), phaseHeight, xBase, sequence +1);
+    }
+    
     /**
      * The main function that will draw a phase. It takes some data from the previous phases,
      * does its thing and recurses onto the next phase.
@@ -67,12 +96,13 @@ function createFunnel(divId, width, height, funnelData) {
                     .attr("fill", "blue");
              
         // add the name of the phase as a title above
-        svgContainer.append("text").attr("x", sequence * getPhaseWidth() + centerText(getPhaseWidth(), 40, currentPhase.name.length))
-                                    .attr("y", headerBuffer - 10)
-                                    .text(currentPhase.name)
-                                    .attr("font-family", "sans-serif")
-                                    .attr("font-size", "50px")
-                                    .attr("fill", "black");
+        var titleX = sequence * getPhaseWidth() + centerText(getPhaseWidth(), 40, currentPhase.name.length);
+        var titleY = headerBuffer - 10;
+        addText(titleX, titleY, 50, currentPhase.name);
+        
+        // now add the entries to the phase
+        addEntriesToPhase(currentPhase.entries, getPhaseHeight(currentPhase), sequence * getPhaseWidth(), 0);
+                           
         // all done ... now recurse to the following element
         mapPhase(tail(phases), getPhaseHeight(currentPhase), sequence + 1);
     }
@@ -86,7 +116,7 @@ function createFunnel(divId, width, height, funnelData) {
 }
 
 /**
- * This function calculates where to position a text in a box based on the size and the length of the text.
+ * This function calculates where to position a text in the center of a box based on the size and the length of the text.
  * When using a font with variable width, the position will be an approximation since a 'I' or 'l' does not take
  * the same amount of space as a 'W'... but in general, it will do
  */
@@ -96,6 +126,38 @@ function centerText(boxSize, textSize, textLength) {
         return calc;
     }
     return 0; // prevent large texts creeping into the left side
+}
+
+/**
+ * This function calculates where to position a text somewhere in a box based on the size and the length of the text.
+ * When using a font with variable width, the position will be an approximation since a 'I' or 'l' does not take
+ * the same amount of space as a 'W'... but in general, it will do. Instead of always centering, it will use Math.random to
+ * pull it more to the left or to the rigt to get a richer distribution...
+ */
+function randomText(boxSize, textSize, textLength) {
+    var calc = (boxSize * Math.random()) - (textLength * textSize / 2);
+    if( calc < 0) {
+        return 0;
+    }
+    return calc; // prevent large texts creeping into the left side
+}
+
+/**
+ * This function calculates the position for a sequence. The first element will be in the middle, the next in the middle of the middle, ...
+ */
+function getYPositionForSequence(sequence, height) {
+    var fraction = Math.log2(sequence+2);
+    var offset =  height / (fraction * 2);
+    
+    if(sequence === 0) {
+        return 0; // first one, dead center
+    } else if(sequence === 1) {
+        return offset;
+    } else if(sequence === 2) {
+        return -offset;
+    }
+    return 0;
+    
 }
 
 function head(arr) {
